a1b08fa454aa0496a5704107001bed5127152a7d
iballa003 <iballa.montero@gmail.com>
2024-10-28 12:14:19 +0000

    Creado clase PreferencesManager

---
+++ app/src/main/java/com/example/contador/PreferencesManager.kt
@@ -0,0 +1,22 @@
+package com.example.contador
+
+import android.content.Context
+import android.content.SharedPreferences
+
+class PreferencesManager(context: Context)  {
+
+    //companion object {
+        private val sharedPreferences: SharedPreferences =
+            context.getSharedPreferences("Preferences", Context.MODE_PRIVATE)
+
+    fun saveData(key: String, value: String) {
+        val editor = sharedPreferences.edit()
+        editor.putString(key, value)
+        editor.apply()
+    }
+
+    fun getData(key: String, defaultValue: String): String {
+        return sharedPreferences.getString(key, defaultValue) ?: defaultValue
+    }
+    //}
+}
